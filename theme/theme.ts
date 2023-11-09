eda59f1d5ccc54202e20e8acef1cf1bf1889c868
simplysabir <simplysabir@gmail.com>
2023-11-09 16:29:36 +0530

    theme added

---
+++ theme/theme.ts
@@ -0,0 +1,19 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+export const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#1F6FFF"
+        }
+    }
+});
+export const lightTheme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#1F6FFF"
+        }
+    }
+});
